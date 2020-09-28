4b3852c7bfd73e01d38c811f700ef2a23bffc2f2
teastman21 <71243994+teastman21@users.noreply.github.com>
2020-09-28 00:01:28 -0400

    added tilt, focus, propff

--- matLabPort.py
+++ matLabPort.py
@@ -12,7 +12,7 @@ Created on Sat Sep 19 21:20:07 2020
 import numpy as np
 import math as math
 import decimal as d
-import cmath as c
+import cmath as cmath
 import matplotlib.pyplot as plt
 
 
@@ -42,7 +42,8 @@ def propTF(u1,L,lam,z):
 def rect(x):
     out = np.abs(x) <= 1/2
     return out
-        
+
+'''
 #page 66 square beam example
 L1 = 0.5
 M = 250
@@ -62,14 +63,165 @@ plt.figure()
 plt.imshow(I1)
 
 u2 = propTF(u1,L1,lam,z)
-print(u2)
 x2=x1
 y2=y1
 I2=np.abs(u2)**2
 
 plt.figure()
 plt.imshow(I2)
+'''
 
+def tilt(uin, L, lam, alpha, theta):
+    """ported from tilt function, Voelz pg 90
+    """
+    m = uin.shape[0]
+    n = uin.shape[1]
+    dx = L/m
+    k = 2 * math.pi / lam
+    #need to add the small fraction to make end range inclusive
+    #solution for a clean fix are write a handmade range function that
+    #includes the end range
+    x = np.arange((-L/2), (L/2)-dx + .0000000000000001, dx)
+    X,Y = np.meshgrid(x, x)
+    uout = uin * np.exp(-1j * k * (X * np.cos(theta) + Y*np.sin(theta)) * np.tan(alpha))
+    return uout
+
+"""
+#page 66 square beam example for TILT
+L1 = 0.5
+M = 250
+dx1 = L1/M
+x1 = np.arange((-L1/2),(L1/2 -dx1), dx1)
+y1 = x1       
+lam = 0.5*10**(-6)
+k=2*math.pi/lam
+w=0.051
+z=2000
+
+X1,Y1 = np.meshgrid(x1,y1)
+u1 = rect(X1/(2*w)) * rect(Y1/(2*w))
+I1=np.abs(u1**2)
+
+plt.figure()
+plt.imshow(I1)
+
+deg = math.pi/180
+alpha = 5.05 * 10 **-5
+theta = 45 * deg
+u1 = tilt(u1,L1,lam,alpha,theta)
+
+u2 = propTF(u1,L1,lam,z)
+x2=x1
+y2=y1
+I2=np.abs(u2)**2
+
+plt.figure()
+plt.imshow(I2)
+"""
+
+
+def focus(uin, L, lam, zf):
+    """ported from focus function Voelz pg 94
+    """
+    m = uin.shape[0]
+    n = uin.shape[1]
+    dx = L/m
+    k = 2 * math.pi / lam
+    x = np.arange((-L/2),L/2-dx + .0000001 ,dx)
+    X,Y = np.meshgrid(x,x)
+    uout = uin * np.exp(1j * k / (2*zf) * (X**2 + Y **2))
+    return uout
+
+
+"""
+#SQ BM TEST FOR FOCUS
+#page 66 square beam example
+L1 = 0.5
+M = 250
+dx1 = L1/M
+x1 = np.arange((-L1/2),(L1/2 -dx1), dx1)
+y1 = x1       
+lam = 0.5*10**(-6)
+k=2*math.pi/lam
+w=0.051
+z=2000
+
+X1,Y1 = np.meshgrid(x1,y1)
+u1 = rect(X1/(2*w)) * rect(Y1/(2*w))
+I1=np.abs(u1**2)
+
+plt.figure()
+plt.imshow(I1)
+
+zf = 2000
+u1 = focus(u1, L1, lam, zf)
+
+u2 = propTF(u1,L1,lam,z)
+x2=x1
+y2=y1
+I2=np.abs(u2)**2
+
+plt.figure()
+plt.imshow(I2)
+"""
+
+def propff(u1,L1,lam,z):
+    """propFF port from pg 80
+    """
+    
+    m = u1.shape[0]
+    n = u1.shape[1]
+    dx1 = L1/m
+    k = 2*math.pi/lam
+    
+    L2 = lam*z/dx1
+    dx2 = lam*z/L1
+    x2 = np.arange(-L2/2, L2/2-dx2, dx2)
+    X2,Y2 = np.meshgrid(x2,x2)
+    c = 1 / (1j * lam * z) * np.exp(1j * k / (2 * z) * (X2 ** 2 + Y2 **2))
+    u2 = c * np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(u1))) * dx1 ** 2
+    return u2
+
+
+''' SQ BM TEST FOR PROPFF '''
+"""
+L1 = 0.5
+M = 250
+dx1 = L1/M
+x1 = np.arange((-L1/2),(L1/2 -dx1), dx1)
+y1 = x1       
+lam = 0.5*10**(-6)
+k=2*math.pi/lam
+w=0.011
+z=2000
+
+X1,Y1 = np.meshgrid(x1,y1)
+u1 = rect(X1/(2*w)) * rect(Y1/(2*w))
+I1=np.abs(u1**2)
+
+plt.figure()
+plt.imshow(I1)
+
+
+u2 = propTF(u1,L1,lam,z)
+u2,L2 = propff(u1,L1,lam,z)
+dx2 = L2/M
+x2=np.arange(-L1/2,L2/2-dx2,dx2)
+y2=x2
+I2=np.abs(u2)**2
+
+plt.figure()
+plt.imshow(I2)
+    
+    """
+    
+
+    
+    
+    
+    
+    
+    
 
 
         
