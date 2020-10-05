e81a6e14e6fb03a7c6f83fb1d9008cc6f34bd636
teastman21 <71243994+teastman21@users.noreply.github.com>
2020-10-05 17:57:59 -0400

    updated temporal focusing model for 1 dimension

--- temporalFocusingModel.py
+++ temporalFocusingModel.py
@@ -31,18 +31,19 @@ thetaDcenter = 0  #diffracted center wavelength
 m = 1           #diffraction order
 w = 0.002       #beam width
 L = 1.05
-M = 2**16
+M = 2**10
 # multiplied these two by a hundred due to CPU memory constraints @ output
-dx = 100*L / M
-dy = 100* L / M
+dx = L / M
+dy = L / M
 x = np.arange(-L/2, L / 2 - dx + .000000000000000001, dx)
 y = np.arange(-L/2, L / 2 - dy + .000000000000000001, dy)
 alpha = 5.0*10**-5 #tilt angle
 k = 2 * math.pi / lamC
+lam1 = 820 * 10 ** (-9)
 
 #determine diffraction angle
 thetaI = np.arcsin(m * lamC * g - np.sin(thetaDcenter))
-
+thetaD = np.arcsin(m * lam1 * g - np.sin(thetaI))
 
 #working with each dimension individually means we dont use this tilt function?
 def tilt(uin, L, lam, alpha, theta):
@@ -62,12 +63,12 @@ def tilt(uin, L, lam, alpha, theta):
 
 
 #initialize beam and apply tilt
-u1x = np.exp(-x ** 2 / (2 * w ** 2))* np.exp(-1j * k * (x * np.cos(thetaI) + y*np.sin(thetaI)) * np.tan(alpha))
-u1y = np.exp(-y ** 2 / (2 * w ** 2))* np.exp(-1j * k * (x * np.cos(thetaI) + y*np.sin(thetaI)) * np.tan(alpha))
-U1X,U1Y = np.meshgrid(u1x,u1y)
+u1x = np.exp(-x ** 2 / (2 * w ** 2))* np.exp(-1j * k * (x * np.sin(thetaD)))
+u1y = np.exp(-y ** 2 / (2 * w ** 2))
+#U1X,U1Y = np.meshgrid(u1x,u1y)
 print(np.shape(u1x))
 #plot beam after tilt applied
-I=np.abs(U1X)**2
+#I=np.abs(U1X)**2
 plt.figure()
 plt.plot(np.abs(u1x)**2)
 
@@ -77,27 +78,27 @@ if isinstance(u1x, np.ndarray):
 else:
     print("No np")
 #propagate to the focal plane (x)
-L2X = lamC * f1 / dx
-dx2 = lamC * f1 / L
+L2X = lam1 * f1 / dx
+dx2 = lam1 * f1 / L
 
 
 #y
-L2Y = lamC * f1 / dx
-dy2 = lamC * f1 / L
+L2Y = lam1 * f1 / dx
+dy2 = lam1 * f1 / L
 
 #create coordinates at focal plane
 xfoc1 = -L2X / 2 + np.arange(0, M - 1) * dx2
 yfoc1  = -L2Y / 2 + np.arange(0, M - 1) * dy2
 
 #propagate beam with the fourier transform
-u2x = 1/(1j * lamC * f1)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u1x))))*dx
-u2y = 1/(1j * lamC * f1)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u1y))))*dy
+u2x = 1/(1j * lam1 * f1)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u1x))))*dx
+u2y = 1/(1j * lam1 * f1)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u1y))))*dy
 
 #plot beam at fourier plane
-U2X,U2Y = np.meshgrid(u2x,u2y)
-I=np.abs(U2X)**2
+#U2X,U2Y = np.meshgrid(u2x,u2y)
+#I=np.abs(U2X)**2
 plt.figure()
-plt.imshow(I)
+plt.plot(np.abs(u2x)**2)
 
 
 
@@ -107,25 +108,25 @@ else:
     print("No np")
     
 #propagate to output focal plane
-L3X = lamC * f2 / dx2
-dx3 = lamC * f2 / L2X
+L3X = lam1 * f2 / dx2
+dx3 = lam1 * f2 / L2X
 
-L3Y = lamC * f2 / dy2
-dy3 = lamC * f2 / L2Y
+L3Y = lam1 * f2 / dy2
+dy3 = lam1 * f2 / L2Y
  
 #create coordinates at output focal plane
 xfoc2 = -L3X / 2 + np.arange(0, M - 1) * dx3
 yfoc2 = -L3Y / 2 + np.arange(0, M - 1) * dy3
 
 #propagate beam with fourier transform
-u3x = 1/(1j * lamC * f2)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u2x))))*dx2 
-u3y = 1/(1j * lamC * f2)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u2y))))*dy2 
+u3x = 1/(1j * lam1 * f2)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u2x))))*dx2 
+u3y = 1/(1j * lam1 * f2)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u2y))))*dy2 
 
 #plot beam at output plane
-U3X,U3Y = np.meshgrid(u3x,u3y)
-I=np.abs(U3X)**2
+#U3X,U3Y = np.meshgrid(u3x,u3y)
+#I=np.abs(U3X)**2
 plt.figure()
-plt.imshow(I)
+plt.plot(np.abs(u3x)**2)
 
 
 if isinstance(u3x, np.ndarray):
