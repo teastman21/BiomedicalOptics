63a9721f5c12a7c75230c0eae63611da4785d9b8
teastman21 <71243994+teastman21@users.noreply.github.com>
2020-10-07 22:14:21 -0400

    added coverage for multiple wavelengths and Y dimension

--- temporalFocusingModel.py
+++ temporalFocusingModel.py
@@ -22,132 +22,108 @@ import math as math
 import matplotlib.pyplot as plt
 
 
-# Define necessary variables
-f1 = 0.5        #collimating lens focal length
-f2 = 9*10**-3   #objective lens focal length
-lamC = 800*10**(-9) #center wavelength
-g = 120000      #grating density
-thetaDcenter = 0  #diffracted center wavelength 
-m = 1           #diffraction order
-w = 0.002       #beam width
-L = 1.05
-M = 2**10
-# multiplied these two by a hundred due to CPU memory constraints @ output
-dx = L / M
-dy = L / M
-x = np.arange(-L/2, L / 2 - dx + .000000000000000001, dx)
-y = np.arange(-L/2, L / 2 - dy + .000000000000000001, dy)
-alpha = 5.0*10**-5 #tilt angle
-k = 2 * math.pi / lamC
-lam1 = 820 * 10 ** (-9)
-
-#determine diffraction angle
-thetaI = np.arcsin(m * lamC * g - np.sin(thetaDcenter))
-thetaD = np.arcsin(m * lam1 * g - np.sin(thetaI))
-
-#working with each dimension individually means we dont use this tilt function?
-def tilt(uin, L, lam, alpha, theta):
-    """ported from tilt function, Voelz pg 90
-    """
-    m = uin.shape[0]
-    n = uin.shape[1]
-    dx = L/m
-    k = 2 * math.pi / lam
-    #need to add the small fraction to make end range inclusive
-    #solution for a clean fix are write a handmade range function that
-    #includes the end range
-    x = np.arange((-L/2), (L/2)-dx + .0000000000000000001, dx)
-    X,Y = np.meshgrid(x, x)
-    uout = uin * np.exp(-1j * k * (X * np.cos(theta) + Y*np.sin(theta)) * np.tan(alpha))
-    return uout
-
-
-#initialize beam and apply tilt
-u1x = np.exp(-x ** 2 / (2 * w ** 2))* np.exp(-1j * k * (x * np.sin(thetaD)))
-u1y = np.exp(-y ** 2 / (2 * w ** 2))
-#U1X,U1Y = np.meshgrid(u1x,u1y)
-print(np.shape(u1x))
-#plot beam after tilt applied
-#I=np.abs(U1X)**2
-plt.figure()
-plt.plot(np.abs(u1x)**2)
-
-
-if isinstance(u1x, np.ndarray):
-    print("yes np")
-else:
-    print("No np")
-#propagate to the focal plane (x)
-L2X = lam1 * f1 / dx
-dx2 = lam1 * f1 / L
-
-
-#y
-L2Y = lam1 * f1 / dx
-dy2 = lam1 * f1 / L
-
-#create coordinates at focal plane
-xfoc1 = -L2X / 2 + np.arange(0, M - 1) * dx2
-yfoc1  = -L2Y / 2 + np.arange(0, M - 1) * dy2
-
-#propagate beam with the fourier transform
-u2x = 1/(1j * lam1 * f1)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u1x))))*dx
-u2y = 1/(1j * lam1 * f1)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u1y))))*dy
-
-#plot beam at fourier plane
-#U2X,U2Y = np.meshgrid(u2x,u2y)
-#I=np.abs(U2X)**2
-plt.figure()
-plt.plot(np.abs(u2x)**2)
-
-
-
-if isinstance(u2x, np.ndarray):
-    print("yes np")
-else:
-    print("No np")
+def prop(lamm): 
+    # Define necessary variables
+    f1 = 0.5        #collimating lens focal length
+    f2 = 9*10**-3   #objective lens focal length
+    lamC = 800*10**(-9) #center wavelength
+    g = 120000      #grating density
+    thetaDcenter = 0  #diffracted center wavelength 
+    m = 1           #diffraction order
+    w = 0.002       #beam width
+    L = 1.05
+    M = 2**10
     
-#propagate to output focal plane
-L3X = lam1 * f2 / dx2
-dx3 = lam1 * f2 / L2X
-
-L3Y = lam1 * f2 / dy2
-dy3 = lam1 * f2 / L2Y
- 
-#create coordinates at output focal plane
-xfoc2 = -L3X / 2 + np.arange(0, M - 1) * dx3
-yfoc2 = -L3Y / 2 + np.arange(0, M - 1) * dy3
-
-#propagate beam with fourier transform
-u3x = 1/(1j * lam1 * f2)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u2x))))*dx2 
-u3y = 1/(1j * lam1 * f2)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u2y))))*dy2 
-
-#plot beam at output plane
-#U3X,U3Y = np.meshgrid(u3x,u3y)
-#I=np.abs(U3X)**2
-plt.figure()
-plt.plot(np.abs(u3x)**2)
-
-
-if isinstance(u3x, np.ndarray):
-    print("yes np")
-else:
-    print("No np")
-#final coordinates
-#fc = np.meshgrid(u3x,u3y)
-    #need to turn fc into x y pairs!
-fc = np.column_stack((u3x,u3y))
-print(np.shape(fc))
-if isinstance(fc, np.ndarray):
-    print("yes np")
-else:
-    print("No np")
+    # multiplied these two by a hundred due to CPU memory constraints @ output
+    dx = L / M
+    dy = L / M
+    x = np.arange(-L/2, L / 2 - dx + .000000000000000001, dx)
+    y = np.arange(-L/2, L / 2 - dy + .000000000000000001, dy)
+    k = 2 * math.pi / lamC
+    #lam1 = 820 * 10 ** (-9)
+    lam1 = lamm
     
-print(len(fc))
-I=np.abs(fc)**2
+    #determine diffraction angle
+    thetaI = np.arcsin(m * lamC * g - np.sin(thetaDcenter))
+    thetaD = np.arcsin(m * lam1 * g - np.sin(thetaI))
+    
+    #initialize beam and apply tilt
+    u1x = np.exp(-x ** 2 / (2 * w ** 2))* np.exp(-1j * k * (x * np.sin(thetaD)))
+    u1y = np.exp(-y ** 2 / (2 * w ** 2))
+    
+    #U1X,U1Y = np.meshgrid(u1x,u1y)
+    #plot beam after tilt applied
+    #I=np.abs(U1X)**2
+    plt.figure()
+    plt.plot(np.abs(u1x)**2)
+    
+    plt.figure()
+    plt.plot(np.abs(u1y)**2)
+    
+    #propagate to the focal plane (x)
+    L2X = lam1 * f1 / dx
+    dx2 = lam1 * f1 / L
+    
+    
+    #y
+    L2Y = lam1 * f1 / dx
+    dy2 = lam1 * f1 / L
+    
+    #create coordinates at focal plane
+    xfoc1 = -L2X / 2 + np.arange(0, M - 1) * dx2
+    yfoc1  = -L2Y / 2 + np.arange(0, M - 1) * dy2
+    
+    #propagate beam with the fourier transform
+    u2x = 1/(1j * lam1 * f1)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u1x))))*dx
+    u2y = 1/(1j * lam1 * f1)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u1y))))*dy
+    
+    #plot beam at fourier plane
+    #U2X,U2Y = np.meshgrid(u2x,u2y)
+    #I=np.abs(U2X)**2
+    plt.figure()
+    plt.plot(np.abs(u2x)**2)
+    
+    plt.figure()
+    plt.plot(np.abs(u2y)**2)
+    
+    
+        
+    #propagate to output focal plane
+    L3X = lam1 * f2 / dx2
+    dx3 = lam1 * f2 / L2X
+    
+    L3Y = lam1 * f2 / dy2
+    dy3 = lam1 * f2 / L2Y
+     
+    #create coordinates at output focal plane
+    xfoc2 = -L3X / 2 + np.arange(0, M - 1) * dx3
+    yfoc2 = -L3Y / 2 + np.arange(0, M - 1) * dy3
+    
+    #propagate beam with fourier transform
+    u3x = 1/(1j * lam1 * f2)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u2x))))*dx2 
+    u3y = 1/(1j * lam1 * f2)*(np.fft.ifftshift(np.fft.fft(np.fft.fftshift(u2y))))*dy2 
+    
+    #plot beam at output plane
+    #U3X,U3Y = np.meshgrid(u3x,u3y)
+    #I=np.abs(U3X)**2
+    plt.figure()
+    plt.plot(np.abs(u3x)**2)
+    
+    plt.figure()
+    plt.plot(np.abs(u3y)**2)
+    
+    
+    #final coordinates
+    #fc = np.meshgrid(u3x,u3y)
+        #need to turn fc into x y pairs!
+    fc = np.column_stack((u3x,u3y))
+    I=np.abs(fc)**2
 
 
-
+wavelengthlist = np.linspace(780*10**(-9),820*10**(-9),num=2)
+for lamb in wavelengthlist:
+    prop(lamb)
+    display()
 
 
 
